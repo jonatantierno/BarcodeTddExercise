bb42a02079d5a37e8947918f08c3345ded877b7a
Jonatan Tierno <jonatan.tierno@rumbo.com>
2015-12-15 00:27:53 +0100

    Now the catalog can read prices and returns null if not present

--- barcode/src/test/java/StorePricesInMap.java
+++ barcode/src/test/java/StorePricesInMap.java
@@ -7,6 +7,7 @@ import com.tdd.training.MapPriceCatalog;
 import com.tdd.training.Price;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StorePricesInMap
@@ -16,9 +17,29 @@ public class StorePricesInMap
 
     @Test
     public void priceFound() {
-        MapPriceCatalog catalog = new MapPriceCatalog();
-        catalog.addPrice("code",price);
+        MapPriceCatalog catalog = createCatalogWithCode("code", price);
 
         assertEquals(price, catalog.findPrice("code"));
+    }
+
+    private MapPriceCatalog createCatalogWithCode(String code, Price price)
+    {
+        MapPriceCatalog catalog = new MapPriceCatalog();
+        catalog.addPrice(code, price);
+        return catalog;
+    }
+
+    @Test
+    public void priceNotFound() {
+        MapPriceCatalog catalog = createcatalogWithout("code");
+
+        assertEquals(null, catalog.findPrice("code"));
+    }
+
+    private MapPriceCatalog createcatalogWithout(String code)
+    {
+        MapPriceCatalog catalog = new MapPriceCatalog();
+        catalog.addPrice("not "+code,mock(Price.class));
+        return catalog;
     }
 }
